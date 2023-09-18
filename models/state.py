03c77013a5437e98b9f53e2b609494c8adc043a8
Benedicta1 <Benedictaojogwu@gmail.com>
2023-09-18 05:29:18 -0700

    State class that inherits from Basemodel

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This defines the State class."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Properties:
+
+        name (str): The name of the state.
+    """
+
+    name = ""
