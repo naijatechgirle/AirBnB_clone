f09a4f8277af240847434dbac7f48a4d88b65037
Benedicta1 <Benedictaojogwu@gmail.com>
2023-09-18 05:44:46 -0700

    Amenity class that inherit from Basemodel

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Properties
+
+    Class Attributes:
+        name (str): This represents  name of the amenity.
+    """
+
+    name = ""
